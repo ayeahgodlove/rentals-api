72a34778c09380075809fa87f92d35898f36636d
Ayeah Godlove <ayeahgodlove5@gmail.com>
2023-02-24 00:04:56 +0100

    feat(db): setup database connection

---
+++ src/adapters/database/postgres/db.config.ts
@@ -0,0 +1,40 @@
+/**
+ * Set-by-step build Node.js Resful CRUD API using Express, Sequelize with MySQL
+ * Sequelize is a promise-based Node.j ORM that supports the dialects for Postgres, MysQL, and SQL server
+ * Sequelize with MySQL
+ */
+import { Sequelize } from "sequelize-typescript";
+
+export class DbConfig {
+    private readonly sequelize!: Sequelize;
+    /**
+     *
+     */
+    constructor() {
+        this.sequelize = new Sequelize({
+            username: "postgres",
+            password: "admin@2022",
+            database: "honeyman_db",
+            port: 5432,
+            host: "localhost",
+            dialect: "postgres",
+            models: [],
+            logging: false,
+            pool: {
+                max: 5,
+                min: 0,
+                acquire: 30000,
+                idle: 10000,
+            },
+        })
+    }
+
+    connection = async () => {
+        try {
+            await this.sequelize.authenticate();
+            console.log("Connection has been established successfully.");
+        } catch (error) {
+            console.error("Unable to connect to the database:", error);
+        }
+    }
+}
