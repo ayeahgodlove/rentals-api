adbf26bbe9ace683eac1eb80fef0be9f4748d49c
Ayeah Godlove <ayeahgodlove5@gmail.com>
2023-03-29 01:09:29 +0100

    feat(usecase): Add lacking usecases

--- src/data/repositories/contracts/ipayment.repository.ts
+++ src/data/repositories/contracts/ipayment.repository.ts
@@ -4,6 +4,7 @@ import { Payment } from "../../entities/payment";
 export interface IPaymentRepository {
     create(payment: IPayment): Promise<Payment>;
     findById(id: string): Promise<Payment | null>;
+    findByOrderId(orderId: string): Promise<Payment | null>;
     getAll(): Promise<Payment[]>;
     update(payment: IPayment): Promise<Payment>;
     delete(id: string): Promise<void>;
