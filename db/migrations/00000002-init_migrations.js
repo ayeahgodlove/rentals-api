07230e0b578aa0619d6bc375ccfad245a044c929
Ayeah Godlove A <ayeahgodlove5@gmail.com>
2023-04-18 13:50:47 +0100

    update migration list

---
+++ db/migrations/00000002-init_migrations.js
@@ -0,0 +1,232 @@
+'use strict';
+
+const Sequelize = require('sequelize');
+
+/**
+ * Actions summary:
+ *
+ * dropTable "review"
+ * dropTable "user"
+ *
+ **/
+
+const info = {
+    "revision": 2,
+    "name": "init-migrations",
+    "created": "2023-04-18T12:49:14.994Z",
+    "comment": ""
+};
+
+const migrationCommands = [
+
+    {
+        fn: "createTable",
+        params: [
+            "SequelizeMigrationsMeta",
+            {
+                "revision": {
+                    "primaryKey": true,
+                    "type": Sequelize.INTEGER
+                },
+                "name": {
+                    "allowNull": false,
+                    "type": Sequelize.STRING
+                },
+                "state": {
+                    "allowNull": false,
+                    "type": Sequelize.JSON
+                },
+            },
+            {}
+        ]
+    },
+    {
+        fn: "bulkDelete",
+        params: [
+            "SequelizeMigrationsMeta",
+            [{
+                revision: info.revision
+            }],
+            {}
+        ]
+    },
+    {
+        fn: "bulkInsert",
+        params: [
+            "SequelizeMigrationsMeta",
+            [{
+                revision: info.revision,
+                name: info.name,
+                state: '{"revision":2,"tables":{"category":{"tableName":"category","schema":{"id":{"seqType":"Sequelize.STRING(50)","allowNull":false,"primaryKey":true},"name":{"seqType":"Sequelize.STRING(128)","allowNull":false,"unique":true},"slug":{"seqType":"Sequelize.STRING(128)","allowNull":false,"unique":true},"description":{"seqType":"Sequelize.TEXT","allowNull":false},"createdAt":{"seqType":"Sequelize.DATE","allowNull":false},"updatedAt":{"seqType":"Sequelize.DATE","allowNull":false},"deletedAt":{"seqType":"Sequelize.DATE"}},"indexes":{}}}}'
+            }],
+            {}
+        ]
+    },
+
+
+
+    {
+        fn: "dropTable",
+        params: ["review"]
+    },
+    {
+        fn: "dropTable",
+        params: ["user"]
+    }
+];
+
+const rollbackCommands = [
+
+    {
+        fn: "bulkDelete",
+        params: [
+            "SequelizeMigrationsMeta",
+            [{
+                revision: info.revision,
+            }],
+            {}
+        ]
+    },
+
+
+
+
+    {
+        fn: "createTable",
+        params: [
+            "user",
+            {
+                "id": {
+                    "primaryKey": true,
+                    "allowNull": false,
+                    "type": Sequelize.STRING(50)
+                },
+                "username": {
+                    "unique": true,
+                    "allowNull": false,
+                    "type": Sequelize.STRING(128)
+                },
+                "fullname": {
+                    "unique": true,
+                    "allowNull": false,
+                    "type": Sequelize.STRING(128)
+                },
+                "email": {
+                    "unique": true,
+                    "allowNull": false,
+                    "type": Sequelize.STRING(128)
+                },
+                "password": {
+                    "unique": true,
+                    "allowNull": false,
+                    "type": Sequelize.STRING(128)
+                },
+                "address": {
+                    "allowNull": false,
+                    "type": Sequelize.STRING(128)
+                },
+                "role": {
+                    "allowNull": false,
+                    "type": Sequelize.STRING(128)
+                },
+                "slug": {
+                    "unique": true,
+                    "allowNull": false,
+                    "type": Sequelize.STRING(128)
+                },
+                "createdAt": {
+                    "allowNull": false,
+                    "type": Sequelize.DATE
+                },
+                "updatedAt": {
+                    "allowNull": false,
+                    "type": Sequelize.DATE
+                },
+                "deletedAt": {
+                    "type": Sequelize.DATE
+                }
+            },
+            {}
+        ]
+    },
+
+    {
+        fn: "createTable",
+        params: [
+            "review",
+            {
+                "id": {
+                    "primaryKey": true,
+                    "allowNull": false,
+                    "type": Sequelize.STRING(50)
+                },
+                "userId": {
+                    "references": {
+                        "model": "user",
+                        "key": "id"
+                    },
+                    "allowNull": false,
+                    "type": Sequelize.STRING(50)
+                },
+                "description": {
+                    "allowNull": false,
+                    "type": Sequelize.TEXT
+                },
+                "rating": {
+                    "allowNull": false,
+                    "type": Sequelize.INTEGER
+                },
+                "createdAt": {
+                    "allowNull": false,
+                    "type": Sequelize.DATE
+                },
+                "updatedAt": {
+                    "allowNull": false,
+                    "type": Sequelize.DATE
+                },
+                "deletedAt": {
+                    "type": Sequelize.DATE
+                }
+            },
+            {}
+        ]
+    }
+];
+
+module.exports = {
+  pos: 0,
+  up: function(queryInterface, Sequelize) {
+    let index = this.pos;
+
+    return new Promise(function(resolve, reject) {
+      function next() {
+        if (index < migrationCommands.length) {
+          let command = migrationCommands[index];
+          console.log("[#"+index+"] execute: " + command.fn);
+          index++;
+          queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
+        } else resolve();
+      }
+
+      next();
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    let index = this.pos;
+
+    return new Promise(function(resolve, reject) {
+      function next() {
+        if (index < rollbackCommands.length) {
+          let command = rollbackCommands[index];
+          console.log("[#"+index+"] execute: " + command.fn);
+          index++;
+          queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
+        }
+        else resolve();
+      }
+
+      next();
+    });
+  },
+  info
+};
